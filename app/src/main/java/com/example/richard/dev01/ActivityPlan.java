7221995b3c81f50d04bd764b375972a768f95d61
Richuo <37586943+Richuo@users.noreply.github.com>
2018-04-25 17:51:54 +0200

    Restructuration du code
    Ajout de méthode et des valeurs dans ressources pour éviter de hard-coder

---
+++ app/src/main/java/com/example/richard/dev01/ActivityPlan.java
@@ -0,0 +1,13 @@
+package com.example.richard.dev01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan);
+    }
+}
--- app/src/main/java/com/example/richard/dev01/MapsActivity.java
+++ app/src/main/java/com/example/richard/dev01/MapsActivity.java
@@ -1,10 +1,14 @@
 package com.example.richard.dev01;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
+
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -21,6 +25,7 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
+
 
 //import com.google.android.gms.maps.model.Marker;
 //import com.google.android.gms.maps.model.MarkerOptions;
@@ -50,6 +55,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Polygon polygonei3;
 
 
+
+
     private CameraPosition cameraPosition;
 
 
@@ -68,6 +75,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -81,10 +89,70 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
 
+
+
+    CameraPosition newcameraposition(LatLng position,int val_zoom) { //Fonction : centrer sur une coordonnée avec une valeur de zoom
+        cameraPosition = new CameraPosition.Builder()
+                .target(position)      // Sets the center of the map to imt
+                .zoom(val_zoom)                   // Sets the zoom
+                .bearing(-24)                // Sets the orientation of the camera to imt north
+                .build();                   // Creates a CameraPosition from the builder
+
+        return(cameraPosition);
+    }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        //INITIALISATION DES COORDONNES//
+        TypedValue typedValue = new TypedValue();
+
+
+        //CENTRE I1
+        getResources().getValue(R.dimen.posi1x, typedValue, true);
+        float posi1x = typedValue.getFloat();
+
+        getResources().getValue(R.dimen.posi1y, typedValue, true);
+        float posi1y = typedValue.getFloat();
+
+        final LatLng posi1 = new LatLng(posi1y,posi1x);
+
+
+        //CENTRE I3
+        getResources().getValue(R.dimen.posi3x, typedValue, true);
+        float posi3x = typedValue.getFloat();
+
+        getResources().getValue(R.dimen.posi3y, typedValue, true);
+        float posi3y = typedValue.getFloat();
+
+        final LatLng posi3 = new LatLng(posi3y,posi3x); //48.357958, -4.571160);
+
+
+        ///RESTRICTIONS///
+
+        //RESTRICTION DE LA CARTE
+        LatLngBounds RESTRICIMT = new LatLngBounds(
+                new LatLng(48.353, -4.5739) , new LatLng(48.363, -4.565));
+        // Constrain the camera target to the IMT Atlantique bounds (BAS-GAUCHE,HAUT-DROITE)
+        // TODO : ROTATION DES LATLNGBOUNDS.
+        mMap.setLatLngBoundsForCameraTarget(RESTRICIMT);
+
+        //RESTRICTION ZOOM
+        mMap.setMaxZoomPreference(19f); //Zoom maximal
+        mMap.setMinZoomPreference(15f); //Zoom Minimal
+        //////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+        ///STYLE DE LA MAP/// (Par défaut pour l'instant)
         try {
             // Customise the styling of the base map using a JSON object defined
             // in a raw resource file.
@@ -98,6 +166,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } catch (Resources.NotFoundException e) {
             Log.e(TAG, "Can't find style. Error: ", e);
         }
+        /////////////////////////////////////////////////////////
 
 
 
@@ -108,33 +177,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .title("IMT Atlantique")
                 .snippet("L'école d'ingénieur la plus proche de New York")
                 .infoWindowAnchor(0.5f, 0.5f));
-
-
-        //RESTRICTION DE LA CARTE
-        LatLngBounds ADELAIDE = new LatLngBounds(
-                new LatLng(48.353, -4.5739) , new LatLng(48.363, -4.565));
-        // Constrain the camera target to the IMT Atlantique bounds (BAS-GAUCHE,HAUT-DROITE)
-        // TODO : ROTATION DES LATLNGBOUNDS.
-        mMap.setLatLngBoundsForCameraTarget(ADELAIDE);
-
-        cameraPosition = new CameraPosition.Builder()
-                .target(imt)      // Sets the center of the map to imt
-                .zoom(17)                   // Sets the zoom
-                .bearing(-24)                // Sets the orientation of the camera to imt north
-                .build();                   // Creates a CameraPosition from the builder
+        ///////////////////////////////////////////////////////////////////
 
 
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(imt,20));
 
-        mMap.animateCamera(CameraUpdateFactory.zoomIn());
 
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(17), 2000, null);
 
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        //Animation au démarrage de la carte
 
-        mMap.setMaxZoomPreference(19f); //Zoom maximal
-        mMap.setMinZoomPreference(15f); //Zoom Minimal
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(imt,10));
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(newcameraposition(imt,17)));
+
+
+
 
 
 
@@ -147,8 +204,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mI1nom = mMap.addGroundOverlay(new GroundOverlayOptions()
                 .image(BitmapDescriptorFactory.fromResource(R.mipmap.nomi1))
                 .bearing(-24)
-                .zIndex(1)
-                .position(new LatLng(48.357574, -4.570841), 15f, 15f)
+                .zIndex(1) //Position z
+                .position(posi1, 15f, 15f)
                 .clickable(true));
 
         mI1nom.setTag("Bâtiment I1");
@@ -159,8 +216,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mI3nom = mMap.addGroundOverlay(new GroundOverlayOptions()
                 .image(BitmapDescriptorFactory.fromResource(R.mipmap.nomi3))
                 .bearing(-24)
-                .zIndex(1)
-                .position(new LatLng(48.357958, -4.571160), 15f, 15f)
+                .zIndex(1) //Position z
+                .position(posi3, 15f, 15f)
                 .clickable(true));
 
         mI3nom.setTag("Bâtiment I3");
@@ -186,19 +243,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .add(new LatLng(48.357947, -4.571288))
                 .add(new LatLng(48.358136, -4.571402))  // Closes the polyline.
                 .clickable(true)
-                .zIndex(0)
-                .fillColor(Color.BLUE);
+                .zIndex(0) //Position z
+                .fillColor(Color.GRAY);
 
+
+        //Action du click sur le polygone
         mMap.setOnPolygonClickListener(new GoogleMap.OnPolygonClickListener() {
+            @Override
             public void onPolygonClick(Polygon polygon) {
 
-                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition)); //TEST
-                //setContentView();
-
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(newcameraposition(posi3,18)));
+                //startActivity(new Intent(MapsActivity.this, ActivityPlan.class));
             }
         });
 
-        // Get back the mutable Polygon
+
+        // Ajout du polygone sur la carte
         polygonei3 = mMap.addPolygon(rectOptions);
 
 
